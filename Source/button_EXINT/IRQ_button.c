599cb59341de700e8b1efd1cb1300f0678bce0a1
fidaje <f.iuorio@studenti.unisannio.it>
2026-01-10 09:46:37 +0100

    added button library

---
+++ Source/button_EXINT/IRQ_button.c
@@ -0,0 +1,30 @@
+#include "button.h"
+#include "LPC17xx.h"
+
+extern int down_1;
+extern int down_2;
+
+void EINT0_IRQHandler (void)	  	/* INT0														 */
+{		
+	
+	LPC_SC->EXTINT &= (1 << 0);     /* clear pending interrupt         */
+}
+
+
+void EINT1_IRQHandler (void)	  	/* KEY1														 */
+{
+	NVIC_DisableIRQ(EINT1_IRQn);		/* disable Button interrupts			 */
+	LPC_PINCON->PINSEL4    &= ~(1 << 22);     /* GPIO pin selection */
+	down_1=1;
+	LPC_SC->EXTINT &= (1 << 1);     /* clear pending interrupt         */
+}
+
+void EINT2_IRQHandler (void)	  	/* KEY2														 */
+{
+	NVIC_DisableIRQ(EINT2_IRQn);
+	LPC_PINCON->PINSEL4 &= ~(1 << 24);  
+	down_2=1;
+  LPC_SC->EXTINT &= (1 << 2);     /* clear pending interrupt         */    
+}
+
+
